eec7cd11540e57d08a6963a12ae3e14e44e1a90d
Jaideep K <jaideep@one2n.in>
2022-11-13 14:33:18 +0530

    Use bitset for inmemory calculation.

--- bloom_test.go
+++ bloom_test.go
@@ -4,9 +4,11 @@ import (
 	"testing"
 	"time"
 
+	"encoding/binary"
+
 	"github.com/alicebob/miniredis"
+	"github.com/bculberson/bloom"
 	"github.com/garyburd/redigo/redis"
-	"github.com/jdk2588/bloom"
 )
 
 func TestRedisBloomFilter(t *testing.T) {
@@ -30,6 +32,45 @@ func TestRedisBloomFilter(t *testing.T) {
 	testBloomFilter(t, b)
 }
 
+func TestBloomFilter(t *testing.T) {
+	m, k := bloom.EstimateParameters(1000, .01)
+	b := bloom.New(m, k, bloom.NewBitSet(m))
+	testBloomFilter(t, b)
+}
+
+func TestCollision(t *testing.T) {
+	n := uint(10000)
+	fp := .01
+	m, k := bloom.EstimateParameters(n, fp)
+	b := bloom.New(m, k, bloom.NewBitSet(m))
+	shouldNotExist := 0
+	for i := uint(0); i < n; i++ {
+		data := make([]byte, 4)
+		binary.LittleEndian.PutUint32(data, uint32(i))
+		existsBefore, err := b.Exists(data)
+		if err != nil {
+			t.Fatal("Error checking existence.")
+		}
+		if existsBefore {
+			shouldNotExist = shouldNotExist + 1
+		}
+		err = b.Add(data)
+		if err != nil {
+			t.Fatal("Error adding item.")
+		}
+		existsAfter, err := b.Exists(data)
+		if err != nil {
+			t.Fatal("Error checking existence.")
+		}
+		if !existsAfter {
+			t.Fatal("Item should exist.")
+		}
+	}
+	if float64(shouldNotExist) > fp*float64(n) {
+		t.Fatal("Too many false positives.")
+	}
+}
+
 func testBloomFilter(t *testing.T, b *bloom.BloomFilter) {
 	data := []byte("some key")
 	existsBefore, err := b.Exists(data)
@@ -49,16 +90,5 @@ func testBloomFilter(t *testing.T, b *bloom.BloomFilter) {
 	}
 	if !existsAfter {
 		t.Fatal("Bloom filter should contain this data")
-	}
-	err = b.Remove(data)
-	if err != nil {
-		t.Fatal("Error removing from bloom filter")
-	}
-	exists, err := b.Exists(data)
-	if err != nil {
-		t.Fatal("Error checking for existence in bloom filter")
-	}
-	if exists {
-		t.Fatal("Bloom filter should not contain this data")
 	}
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,9 @@ require (
 
 require (
 	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
+	github.com/bculberson/bloom v0.0.0-20170119034211-8ae6b4d72da4 // indirect
+	github.com/bits-and-blooms/bitset v1.3.3 // indirect
 	github.com/gomodule/redigo v1.8.9 // indirect
+	github.com/willf/bitset v1.1.11 // indirect
 	github.com/yuin/gopher-lua v0.0.0-20220504180219-658193537a64 // indirect
 )
